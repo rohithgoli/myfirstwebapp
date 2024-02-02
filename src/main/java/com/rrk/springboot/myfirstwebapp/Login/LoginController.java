75318aee62f0c3ef148d24954c977adacc652a60
Rohith Goli <rohithgoli396@gmail.com>
2024-02-02 15:32:21 +0530

    feat: added logger and its configuration

--- src/main/java/com/rrk/springboot/myfirstwebapp/Login/LoginController.java
+++ src/main/java/com/rrk/springboot/myfirstwebapp/Login/LoginController.java
@@ -1,5 +1,8 @@
 package com.rrk.springboot.myfirstwebapp.Login;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +11,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
 	@RequestMapping("login")
 	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
 		model.put("name", name);
+		logger.debug("Request Param is {}", name);
 		return "login";
 	}
 	
