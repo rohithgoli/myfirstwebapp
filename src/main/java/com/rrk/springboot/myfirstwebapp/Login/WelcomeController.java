661351c79697195e9456c01229006e29b358b330
Rohith Goli <rohithgoli396@gmail.com>
2024-02-05 16:34:52 +0530

    feat: added SpringSecurityConfiguration with InMemoryUserDetailsManager and BCryptPasswordEncoder

---
+++ src/main/java/com/rrk/springboot/myfirstwebapp/Login/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.rrk.springboot.myfirstwebapp.Login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("name")
+public class WelcomeController {
+	
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String gotoWelcomePage(ModelMap model) {
+		model.put("name", "Rohith");
+		return "home";
+	}
+}
