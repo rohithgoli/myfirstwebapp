e452a63b01c7cdb0b91668e44c4a2cf074af4734
Rohith Goli <rohithgoli396@gmail.com>
2024-02-06 10:52:16 +0530

    feat: added display todos list from Database using Repository with Spring Data JPA

--- src/main/java/com/rrk/springboot/myfirstwebapp/todo/TodoControllerJpa.java
+++ src/main/java/com/rrk/springboot/myfirstwebapp/todo/TodoControllerJpa.java
@@ -15,21 +15,25 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 import jakarta.validation.Valid;
 
-//@Controller
+@Controller
 @SessionAttributes("name")
-public class TodoController {
+public class TodoControllerJpa {
 	
 	private TodoService todoService;
 	
-	public TodoController(TodoService todoService) {
+	private TodoRepository todoRepository;
+	
+	public TodoControllerJpa(TodoService todoService, TodoRepository todoRepository) {
 		super();
 		this.todoService = todoService;
+		this.todoRepository = todoRepository;
 	}
 
 	@RequestMapping("list-todos")
 	public String listAllTodos(ModelMap model) {
 		String username = getLoggedInUsername(model);
-		List<Todo> todos = todoService.findByUsername(username);
+		
+		List<Todo> todos = todoRepository.findByUsername(username);
 		model.addAttribute("todos", todos);
 		return "listTodos";
 	}
