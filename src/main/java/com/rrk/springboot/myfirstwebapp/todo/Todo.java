00cb1490c6d21048d5289914e23dcc3a7bb60586
Rohith Goli <rohithgoli396@gmail.com>
2024-02-06 10:06:53 +0530

    feat: update Todo Bean as Entity to map it to table in database with JPA

--- src/main/java/com/rrk/springboot/myfirstwebapp/todo/Todo.java
+++ src/main/java/com/rrk/springboot/myfirstwebapp/todo/Todo.java
@@ -2,11 +2,18 @@ package com.rrk.springboot.myfirstwebapp.todo;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class Todo {
 	
+	@Id
+	@GeneratedValue
 	private int id;
+	
 	private String username;
 	@Size(min=10, message="Enter atleast 10 characters")
 	private String description;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001, 'Ram', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002, 'Ram', 'Get Azure Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003, 'Ram', 'Get GCP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004, 'Ram', 'Learn DevOps', CURRENT_DATE(), false);
