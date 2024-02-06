20831ed02fec057cbaf837f5b2a3305ab70bed60
Rohith Goli <rohithgoli396@gmail.com>
2024-02-06 11:25:53 +0530

    feat: added performing CRUD operations with JpaRepository without using static todos from service

--- src/main/java/com/rrk/springboot/myfirstwebapp/todo/TodoControllerJpa.java
+++ src/main/java/com/rrk/springboot/myfirstwebapp/todo/TodoControllerJpa.java
@@ -19,13 +19,10 @@ import jakarta.validation.Valid;
 @SessionAttributes("name")
 public class TodoControllerJpa {
 	
-	private TodoService todoService;
-	
 	private TodoRepository todoRepository;
 	
-	public TodoControllerJpa(TodoService todoService, TodoRepository todoRepository) {
+	public TodoControllerJpa(TodoRepository todoRepository) {
 		super();
-		this.todoService = todoService;
 		this.todoRepository = todoRepository;
 	}
 
@@ -54,20 +51,23 @@ public class TodoControllerJpa {
 		}
 		
 		String username = getLoggedInUsername(model);
-		todoService.addTodo(username, todo.getDescription(), 
-				todo.getTargetDate(), false);
+		todo.setUsername(username);
+		todoRepository.save(todo);
+//		todoService.addTodo(username, todo.getDescription(), 
+//				todo.getTargetDate(), todo.isDone());
 		return "redirect:list-todos";
 	}
 	
 	@RequestMapping("delete-todo")
 	public String deleteTodo(@RequestParam int id) {
-		todoService.deleteById(id);
+		todoRepository.deleteById(id);
+		//		todoService.deleteById(id);
 		return "redirect:list-todos";
 	}
 	
 	@RequestMapping(value="update-todo", method=RequestMethod.GET)
 	public String showUpdateTodoPage(@RequestParam int id, ModelMap model) {
-		Todo todo = todoService.findById(id);
+		Todo todo = todoRepository.findById(id).get();
 		model.addAttribute("todo", todo);
 		return "todo";
 	}
@@ -81,7 +81,7 @@ public class TodoControllerJpa {
 		
 		String username = getLoggedInUsername(model);
 		todo.setUsername(username);
-		todoService.updateTodo(todo);
+		todoRepository.save(todo);
 		return "redirect:list-todos";
 	}
 	
